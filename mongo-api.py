faee78ac28fe3994e6b180f9dde316926f45f90a
Juanfelon14 <j.londonog@uniandes.edu.co>
2026-05-16 19:12:05 -0500

    Refactor MongoDB connection and comment retrieval

--- mongo-api.py
+++ mongo-api.py
@@ -13,40 +13,37 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-#os.environ para despliegue. Descomente cuando ya probó todo local.
+# Conexión MongoDB
 client = MongoClient(os.environ["MONGO_URI"])
-# TODO: conectarse al cluster Admonsis
-client = MongoClient("mongodb://ISIS2304D08202610:tsnrqjihUefx@157.253.236.88:8087")
 
-client = MongoClient("")
-# TODO: conectarse a la base de datos Admonsis  
+# Base de datos
 db = client["ISIS2304D08202610"]
-db = client[""]
 
 
 @app.get("/")
 def inicio():
     return {"estado": "API funcionando correctamente"}
 
+
 @app.get('/bares/{bar_id}/comentarios')
 def get_comentarios(bar_id: int):
-    comentarios = list(db["comentarios"].find(
-        {"bar_id": bar_id},
-        {"_id": 0}
-    ))
+
+    comentarios = list(
+        db["comentarios"].find(
+            {"bar_id": bar_id},
+            {"_id": 0}
+        )
+    )
+
     return comentarios
+
 
 @app.post('/bares/{bar_id}/comentarios')
 def post_comentario(bar_id: int, datos: dict):
+
     datos['bar_id'] = bar_id
-    datos['fecha']  = datetime.now().isoformat()
-    # TODO: completar
+    datos['fecha'] = datetime.now().isoformat()
+
     db["comentarios"].insert_one(datos)
+
     return {'mensaje': 'Comentario guardado'}
-
-# TODO: implementar GET /bares/{bar_id}/eventos
-# Debe retornar todos los eventos del bar desde la colección 'eventos'
-
-# TODO: implementar POST /bares/{bar_id}/eventos  
-# Debe insertar el evento en la colección 'eventos'
-# Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
